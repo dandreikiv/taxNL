726dbd4c3cad707492e1b39c66f6a24b2aa9167e
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2016-06-23 19:59:50 +0200

    Initial Commit

---
+++ taxNL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taxNL
+//
+//  Created by dmytro.andreikiv@philips.com on 23/06/16.
+//  Copyright © 2016 A-Level. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
